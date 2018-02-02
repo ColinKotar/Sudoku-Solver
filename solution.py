514be3950cca64e695244f75f330ebca35b6b7ee
Colin Skotarski <colinkotar@gmail.com>
2018-02-01 22:32:05 -0500

    only_choice function

--- solution.py
+++ solution.py
@@ -95,8 +95,12 @@ def only_choice(values):
     -----
     You should be able to complete this function by copying your code from the classroom
     """
-    # TODO: Copy your code from the classroom to complete this function
-    raise NotImplementedError
+    for unit in unitlist:
+        for digit in '123456789':
+            dplaces = [box for box in unit if digit in values[box]]
+            if len(dplaces) == 1:
+                values[dplaces[0]] = digit
+    return values
 
 
 def reduce_puzzle(values):
