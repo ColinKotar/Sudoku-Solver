1d17d6e317b588bf9d6d13bea5d7a7541ae0a885
Colin Skotarski <colinkotar@gmail.com>
2018-02-13 17:07:09 -0500

    naked_twins update

--- solution.py
+++ solution.py
@@ -11,7 +11,6 @@ units = extract_units(unitlist, boxes)
 peers = extract_peers(units, boxes)
 
 def naked_twins(values):
-
     """Eliminate values using the naked twins strategy.
 
     Parameters
@@ -31,49 +30,44 @@ def naked_twins(values):
     pairs remaining -- the project assistant test suite will accept either
     convention.
     """
+    from collections import Counter
 
-    val = {"B1": "5", "I5": "35678", "H5": "35678", "C1": "4678",
-        "H6": "23568", "C5": "2", "G2": "123568", "H1": "2346789",
-        "E4": "234678", "G5": "35678", "E2": "123568", "C4": "9",
-        "C7": "13456", "D6": "23568", "A4": "5", "H2": "23568",
-        "D5": "1356789", "I3": "25678", "C6": "368", "D3": "25689",
-        "A1": "2468", "E6": "23568", "B2": "2", "A5": "3468",
-        "E5": "13456789", "F2": "7", "I7": "9", "B5": "46",
-        "F4": "23468", "E3": "25689", "H7": "234568", "H8": "234567",
-        "F6": "23568", "A6": "7", "D9": "1235678", "F7": "1234568",
-        "I8": "123567", "I6": "4", "A8": "2346", "F5": "1345689",
-        "B7": "7", "G3": "245678", "I1": "123678", "G6": "9",
-        "H4": "1", "I4": "23678", "F8": "1234569", "B8": "8",
-        "A3": "1", "E1": "123689", "C3": "4678", "G9": "12345678",
-        "I9": "1235678", "B9": "9", "F9": "1234568", "E8": "12345679",
-        "F1": "123689", "D2": "4", "D4": "23678", "B6": "1",
-        "I2": "123568", "B4": "46", "G7": "1234568", "H3": "2456789",
-        "D8": "1235679", "E7": "1234568", "A2": "9", "D7": "123568",
-        "C9": "13456", "B3": "3", "G8": "1234567", "C8": "13456",
-        "D1": "123689", "A7": "2346", "F3": "25689", "H9": "2345678",
-        "E9": "12345678", "A9": "2346", "G1": "1234678", "C2": "68", "G4": "23678"}
+    # val = {"B1": "5", "I5": "35678", "H5": "35678", "C1": "4678", "H6": "23568", "C5": "2", "G2": "123568", "H1": "2346789",
+    #     "E4": "234678", "G5": "35678", "E2": "123568", "C4": "9", "C7": "13456", "D6": "23568", "A4": "5", "H2": "23568",
+    #     "D5": "1356789", "I3": "25678", "C6": "368", "D3": "25689", "A1": "2468", "E6": "23568", "B2": "2", "A5": "3468",
+    #     "E5": "13456789", "F2": "7", "I7": "9", "B5": "46", "F4": "23468", "E3": "25689", "H7": "234568", "H8": "234567",
+    #     "F6": "23568", "A6": "7", "D9": "1235678", "F7": "1234568", "I8": "123567", "I6": "4", "A8": "2346", "F5": "1345689",
+    #     "B7": "7", "G3": "245678", "I1": "123678", "G6": "9", "H4": "1", "I4": "23678", "F8": "1234569", "B8": "8",
+    #     "A3": "1", "E1": "123689", "C3": "4678", "G9": "12345678", "I9": "1235678", "B9": "9", "F9": "1234568", "E8": "12345679",
+    #     "F1": "123689", "D2": "4", "D4": "23678", "B6": "1", "I2": "123568", "B4": "46", "G7": "1234568", "H3": "2456789",
+    #     "D8": "1235679", "E7": "1234568", "A2": "9", "D7": "123568", "C9": "13456", "B3": "3", "G8": "1234567", "C8": "13456",
+    #     "D1": "123689", "A7": "2346", "F3": "25689", "H9": "2345678", "E9": "12345678", "A9": "2346", "G1": "1234678", "C2": "68", "G4": "23678"}
 
-    display(val)
+    for unit in unitlist:
+        # find the naked twins in each unit
+        unit_keys_values = {k:values[k] for k in unit}
+        count = Counter(unit_keys_values.values())
+        possible_twins = []
+        # find twin value
+        for pt, c in count.items():
+            if c == 2 and len(pt) == 2:
+                possible_twins.append(pt)
+        # if there is a twin in current unit
+        if possible_twins:
+            # find locations of the twins
+            twin_key = {}
+            for key_box, value in unit_keys_values.items():
+                for pt in possible_twins:
+                    if value == pt and value not in twin_key.values():
+                        twin_key[key_box] = pt
+            # using locations, remove twin values from it's peers
+            for k, v in twin_key.items():
+                for p in peers[k]:
+                    if v != values[p]:
+                        for n in v:
+                            values[p] = values[p].replace(n, '')
 
-    # cycle through all possible keys
-    for key in val.keys():
-        # find the naked twins in each set of curr_key_values
-        curr_peers = [k for k in peers[key]]
-        curr_key_values = {k:val[k] for k in curr_peers}
-        count = Counter(curr_key_values.values())
-        # if there is a twin, find twin
-        if 2 in count.values():
-            twin = ''
-            for k, v in count.items():
-                if v == 2 and len(k) == 2:
-                    twin = k
-            for k, v in curr_key_values.items():
-                if v in twin and v != twin:
-                    for n in twin:
-                        val[k] = val[k].replace(n, '')
-
-    display(val)
-
+    return values
 
 
 
