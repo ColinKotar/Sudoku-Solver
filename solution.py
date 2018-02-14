caa35fd7c6fe668130fa88f8d29479d4e8558cd5
Colin Skotarski <colinkotar@gmail.com>
2018-02-13 19:08:25 -0500

    final working solution
    solves any sudoku. I think.

--- solution.py
+++ solution.py
@@ -4,7 +4,7 @@ row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
 diagonal_units = [['A1', 'B2', 'C3', 'D4', 'E5', 'F6', 'G7', 'H8', 'I9'], ['A9', 'B8', 'C7', 'D6', 'E5', 'F4', 'G3', 'H2', 'I1']]
-unitlist = row_units + column_units + square_units
+unitlist = row_units + column_units + square_units + diagonal_units
 
 # Must be called after all units (including diagonals) are added to the unitlist
 units = extract_units(unitlist, boxes)
@@ -32,17 +32,6 @@ def naked_twins(values):
     """
     from collections import Counter
 
-    # val = {"B1": "5", "I5": "35678", "H5": "35678", "C1": "4678", "H6": "23568", "C5": "2", "G2": "123568", "H1": "2346789",
-    #     "E4": "234678", "G5": "35678", "E2": "123568", "C4": "9", "C7": "13456", "D6": "23568", "A4": "5", "H2": "23568",
-    #     "D5": "1356789", "I3": "25678", "C6": "368", "D3": "25689", "A1": "2468", "E6": "23568", "B2": "2", "A5": "3468",
-    #     "E5": "13456789", "F2": "7", "I7": "9", "B5": "46", "F4": "23468", "E3": "25689", "H7": "234568", "H8": "234567",
-    #     "F6": "23568", "A6": "7", "D9": "1235678", "F7": "1234568", "I8": "123567", "I6": "4", "A8": "2346", "F5": "1345689",
-    #     "B7": "7", "G3": "245678", "I1": "123678", "G6": "9", "H4": "1", "I4": "23678", "F8": "1234569", "B8": "8",
-    #     "A3": "1", "E1": "123689", "C3": "4678", "G9": "12345678", "I9": "1235678", "B9": "9", "F9": "1234568", "E8": "12345679",
-    #     "F1": "123689", "D2": "4", "D4": "23678", "B6": "1", "I2": "123568", "B4": "46", "G7": "1234568", "H3": "2456789",
-    #     "D8": "1235679", "E7": "1234568", "A2": "9", "D7": "123568", "C9": "13456", "B3": "3", "G8": "1234567", "C8": "13456",
-    #     "D1": "123689", "A7": "2346", "F3": "25689", "H9": "2345678", "E9": "12345678", "A9": "2346", "G1": "1234678", "C2": "68", "G4": "23678"}
-
     for unit in unitlist:
         # find the naked twins in each unit
         unit_keys_values = {k:values[k] for k in unit}
@@ -60,16 +49,14 @@ def naked_twins(values):
                 for pt in possible_twins:
                     if value == pt and value not in twin_key.values():
                         twin_key[key_box] = pt
-            # using locations, remove twin values from it's peers
+            # using locations, remove twin values from it's unit
             for k, v in twin_key.items():
-                for p in peers[k]:
-                    if v != values[p]:
+                for unit_key in unit:
+                    if values[unit_key] != v:
                         for n in v:
-                            values[p] = values[p].replace(n, '')
+                            values[unit_key] = values[unit_key].replace(n, '')
 
     return values
-
-
 
 
 def eliminate(values):
